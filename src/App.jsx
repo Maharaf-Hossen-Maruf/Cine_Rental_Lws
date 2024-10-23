54528669cb9b88d6b1380f2ead0e333020568264
Maharaf-Hossen-Maruf <maharafhossen103@gmail.com>
2024-10-23 12:51:02 +0600

    Movie Details Page Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import "./App.css";
 
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 import SideBar from "./components/SideBar";
@@ -14,6 +15,7 @@ function App() {
           <MovieList/>
         </div>
       </main>
+      <Footer/>
     </>
   );
 }

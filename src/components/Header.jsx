def89c3e9490a747e1261426cde762b0b470e650
Maharaf-Hossen-Maruf <maharafhossen103@gmail.com>
2024-10-23 15:14:47 +0600

    Cart Template Setup Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
-import logo from '../assets/logo.svg'
-import ring from '../assets/ring.svg'
-import moon from '../assets/icons/moon.svg'
-import shoppingCart from '../assets/shopping-cart.svg'
-
+import { useState } from "react";
+import moon from "../assets/icons/moon.svg";
+import logo from "../assets/logo.svg";
+import ring from "../assets/ring.svg";
+import shoppingCart from "../assets/shopping-cart.svg";
+import Cart_Details from "./Cart_Details";
 
 export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+
+  function handleShowCart() {
+    setShowCart(true);
+  }
+
   return (
     <header>
+      {showCart && <Cart_Details onClose={() => setShowCart(false)} />}
+
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={logo} width="139" height="26" alt="" />
@@ -26,25 +35,16 @@ export default function Header() {
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img
-                src={moon}
-                width="24"
-                height="24"
-                alt=""
-              />
+              <img src={moon} width="24" height="24" alt="" />
             </a>
           </li>
           <li>
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={handleShowCart}
             >
-              <img
-                src={shoppingCart}
-                width="24"
-                height="24"
-                alt=""
-              />
+              <img src={shoppingCart} width="24" height="24" alt="" />
             </a>
           </li>
         </ul>
